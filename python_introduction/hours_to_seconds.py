dd81b58917534639d438b54f7ce9fb48b0b122ba
abdelrahmanmaherSolimanElged <AbdelrahmanMaherSolimanElged@outlook.com>
2024-09-15 13:35:59 +0300

    Week 3 tasks_correct_hour_error

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,11 +2,9 @@
 
 hours = 2  # Number of hours to convert
 
-# Conversion factor (seconds in an hour)
-seconds_per_hour = 3600
 
 # Calculate total seconds
-seconds = hours * seconds_per_hour
+seconds = hours * 3600
 
 # Print the result
 print(f"{hours} hour(s) is {seconds} seconds.") 
