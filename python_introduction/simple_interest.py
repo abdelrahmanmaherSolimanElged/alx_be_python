ab621f39ce10b6ef797087c9bd5445c3647ceabf
abdelrahmanmaherSolimanElged <AbdelrahmanMaherSolimanElged@outlook.com>
2024-09-15 13:27:05 +0300

    Week 3 tasks_v2

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# simple_interest.py
+
+# Define variables
+principal = 1000  # Principal amount (initial investment)
+rate = 0.05       # Annual interest rate (as a decimal)
+time = 3           # Time the money is invested for (in years)
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print the calculated interest
+print("The simple interest is:", interest)
